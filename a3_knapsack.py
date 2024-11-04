a56be96afb9c2b62e382e5baafaddd6b38877f64
coe123 <muchadoboutnoting@gmail.com>
2024-11-04 10:32:20 +0530

    Update a3_knapsack.py
    more shorter

--- a3_knapsack.py
+++ a3_knapsack.py
@@ -1,43 +1,31 @@
-def greedy(arr, capacity):
-	total_profit = 0
-	for item in arr:
-    	if item[2] <= capacity:
-        	capacity -= item[2]
-        	total_profit += item[1]
-        	item.append(1)
-    	else:
-        	fraction = capacity / item[2]
-        	total_profit += item[1] * fraction
-        	item.append(fraction)
-        	break
-	print("Total Profit:", total_profit)
-	print("------------------------------------------")
-	for item in arr:
-    	if len(item) > 4:
-        	print(f"Object Inserted: {item[0]} Fraction: {item[4]}")
-        	print("------------------------------------------")
+def knapsack(val, weights, capacity):
+    n = len(val)
+    dp = [[0 for _ in range(capacity + 1)] for _ in range (n+1)]
 
-n = int(input("Enter the number of objects: "))
-print("------------------------------------------")
-processes = []
-capacity = int(input("Enter the capacity of the knapsack: "))
-print("------------------------------------------")
+    for i in range(1, n + 1):
+        for w in range(capacity + 1):
+            if weights[i - 1] <= w:
+                dp[i][w] = max(dp[i-1][w], dp[i-1][w - weights[i-1]] + val[i - 1])
+            else :
+                dp[i][w] = dp[i - 1][w]
 
+    return dp[n][capacity]
+
+
+n = int(input("Enter number of items  :"))
+val = []
+weights = []
+
+print("Enter the values of the items : ")
 for i in range(n):
-    process = []
-    process.append(i + 1)
-    profit = int(input("Enter the Profit of object: "))
-    process.append(profit)
-    weight = int(input("Enter the Weight of object: "))
-    process.append(weight)
-    process.append(profit / weight)
-    processes.append(process)
+    value = int(input(f"Enter value of item {i+1}"))
+    val.append(value)
 
-print("------------------------------------------")
-print(f" ID\tPROFIT\tWEIGHT\tRATIO")
-for i in processes:
-	print(f" {i[0]}\t{i[1]}\t{i[2]}\t{i[3]}")
-print("------------------------------------------")
+print("Enter the weights of the items : ")
+for i in range(n):
+    weight = int(input(f"Enter value of item {i+1}"))
+    weights.append(weight)
 
-processes.sort(key=lambda x: x[3], reverse=True)
-greedy(processes, capacity)
+capacity = int(input("Enter the capacity of knapsack : "))
+
+print("Maximum capacity of knapsack is : ", knapsack(val, weights, capacity))
