f02ef2613dee1aa2ef2afc5564990b502dee2237
coe123 <muchadoboutnoting@gmail.com>
2024-11-05 18:47:52 +0530

    Update a3_knapsack.py

--- a3_knapsack.py
+++ a3_knapsack.py
@@ -1,21 +1,21 @@
 def greedy(arr, capacity):
-	total_profit = 0
-	for item in arr:
-    	if item[2] <= capacity:
-        	capacity -= item[2]
-        	total_profit += item[1]
-        	item.append(1)
-    	else:
-        	fraction = capacity / item[2]
-        	total_profit += item[1] * fraction
-        	item.append(fraction)
-        	break
-	print("Total Profit:", total_profit)
-	print("------------------------------------------")
-	for item in arr:
-    	if len(item) > 4:
-        	print(f"Object Inserted: {item[0]} Fraction: {item[4]}")
-        	print("------------------------------------------")
+    total_profit = 0
+    for item in arr:
+        if item[2] <= capacity:
+            capacity -= item[2]
+            total_profit += item[1]
+            item.append(1)
+        else:
+            fraction = capacity / item[2]
+            total_profit += item[1] * fraction
+            item.append(fraction)
+            break
+    print("Total Profit:", total_profit)
+    print("------------------------------------------")
+    for item in arr:
+        if len(item) > 4:
+            print(f"Object Inserted: {item[0]} Fraction: {item[4]}")
+            print("------------------------------------------")
 
 n = int(input("Enter the number of objects: "))
 print("------------------------------------------")
@@ -36,7 +36,7 @@ for i in range(n):
 print("------------------------------------------")
 print(f" ID\tPROFIT\tWEIGHT\tRATIO")
 for i in processes:
-	print(f" {i[0]}\t{i[1]}\t{i[2]}\t{i[3]}")
+    print(f" {i[0]}\t{i[1]}\t{i[2]}\t{i[3]}")
 print("------------------------------------------")
 
 processes.sort(key=lambda x: x[3], reverse=True)
